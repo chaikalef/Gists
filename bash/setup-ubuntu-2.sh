c82e4b79c3b1700aafa931cc84f7134d162a24c6
Sergey Chaika <sergeychaika@cps.ai>
2020-08-02 19:07:35 +0300

    Create setup-ubuntu-2.sh

---
+++ bash/setup-ubuntu-2.sh
@@ -0,0 +1,1 @@
+. ~/.bashrc && conda config --set auto_activate_base True && conda config --add channels conda-forge && conda config --add channels defaults && cc main python -y && ca main && cif && ca base && cc web python -y && ca web && ciw && ca base && cc tf python -y && ca tf && ciftc && ca base && cc torch python -y && ca torch && cifpg && ca main
